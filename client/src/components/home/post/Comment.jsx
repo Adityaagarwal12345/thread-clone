cdef61e554fbac791d80a1a3b68fcd8583fc45d6
Aditya Agarwal <agarwaladitya512@gmail.com>
2025-11-10 14:50:51 +0530

    done with navbar connection main menu connection single post and comments

--- client/src/components/home/post/Comment.jsx
+++ client/src/components/home/post/Comment.jsx
@@ -1,60 +1,141 @@
-import { Avatar,Menu,MenuItem,Stack, Typography } from "@mui/material";
-import {IoIoMore} from'react-icons/io';
-import { useMediaQuery } from "@mui/material";
-import { useSelector } from "react-redux";  
-import { use, useState } from "react";
-const Comments=()=>{
-     const { darkMode } = useSelector((state) => state.service);
-    const _700=useMediaQuery("(min-width:700px)");
-    
-    const [anchorEl,setAnchorEl]=useState(null);
+import {
+  Avatar,
+  Menu,
+  MenuItem,
+  Stack,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { IoIosMore } from "react-icons/io";
+import { useSelector } from "react-redux";
+import {
+  useDeleteCommentMutation,
+  useSinglePostQuery,
+} from "../../../redux/service";
+import { Bounce, toast } from "react-toastify";
+import { FaBedPulse } from "react-icons/fa6";
 
-    const handleDeleteComment=()=>{}
-    const handleClose=()=>{
-        setAnchorEl(null);
+const Comments = ({ e, postId }) => {
+  const { darkMode, myInfo } = useSelector((state) => state.service);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [isAdmin, setIsAdmin] = useState();
+
+  const _700 = useMediaQuery("(min-width:700px)");
+
+  const [deleteComment, deleteCommentData] = useDeleteCommentMutation();
+  const { refetch } = useSinglePostQuery(postId);
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDeleteComment = async () => {
+    const info = {
+      postId,
+      id: e?._id,
+    };
+    await deleteComment(info);
+    handleClose();
+    refetch();
+  };
+
+  const checkIsAdmin = () => {
+    if (e && myInfo) {
+      if (e.admin._id === myInfo._id) {
+        setIsAdmin(true);
+        return;
+      }
     }
-    return (<>
-        <Stack flexDirection={"column"}
-        gap={2}
-        
+    setIsAdmin(false);
+  };
+
+  useEffect(() => {
+    checkIsAdmin();
+  }, []);
+
+  useEffect(() => {
+    if (deleteCommentData.isSuccess) {
+      toast.success(deleteCommentData.data.msg, {
+        position: "top-center",
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "colored",
+        transition: Bounce,
+      });
+    }
+    if (deleteCommentData.isError) {
+      toast.error(deleteCommentData.error.data.msg, {
+        position: "top-center",
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "colored",
+        transition: Bounce,
+      });
+    }
+  }, [deleteCommentData.isSuccess, deleteCommentData.isError]);
+
+  return (
+    <>
+      <Stack
+        flexDirection={"row"}
         justifyContent={"space-between"}
         px={2}
         pb={4}
         borderBottom={"1px solid gray"}
         mx={"auto"}
-        width={'90%'}
-        >
-            <Stack flexDirection={"row"}
-
-        alignItems={"center"}
-        
-        gap={_700 ? 2:1}>
-            <Avatar/>
-            <Stack flexDirection={'column'}>
-                <Typography variant="h6" fontWeight={"bold"}
-                fontsize={"0.9rem"}>aditya agd</Typography>
-                 <Typography variant="subtitle2" 
-                fontsize={"0.9rem"}>aditya agd</Typography>
-            </Stack>
-            </Stack>
-            <Stack flexDirection={'row'} gap={1}
-            alignItems={'center'}
-            color={darkMode?"white":"GrayText"}
-            fontSize={"0.8rem"}>
-                <p>24min</p>
-                <IoIosMore size={_700 ?28:20}
-                onClick={(e)=>setAnchorEl(e.currentTarget)}/>
-            </Stack>
+        width={"90%"}
+      >
+        <Stack flexDirection={"row"} gap={_700 ? 2 : 1}>
+          <Avatar
+            src={e ? e.admin.profilePic : ""}
+            alt={e ? e.admin.userName : ""}
+          />
+          <Stack flexDirection={"column"}>
+            <Typography variant="h6" fontWeight={"bold"} fontSize={"0.9rem"}>
+              {e ? e.admin.userName : ""}
+            </Typography>
+            <Typography variant="subtitle2" fontSize={"0.9rem"}>
+              {e ? e.text : ""}
+            </Typography>
+          </Stack>
         </Stack>
-        <Menu 
-        achorEl={""}
-        open={anchorEl !==null?true:false}
+        <Stack
+          flexDirection={"row"}
+          gap={1}
+          alignItems={"center"}
+          color={darkMode ? "white" : "GrayText"}
+          fontSize={"0.9rem"}
+        >
+          <p>24min</p>
+          {isAdmin ? (
+            <IoIosMore
+              size={_700 ? 28 : 20}
+              className="image-icon"
+              onClick={(e) => setAnchorEl(e.currentTarget)}
+            />
+          ) : (
+            <IoIosMore size={_700 ? 28 : 20} className="image-icon" />
+          )}
+        </Stack>
+      </Stack>
+      <Menu
+        anchorEl={anchorEl}
+        open={anchorEl !== null ? true : false}
         onClose={handleClose}
-        anchorOrigin={{vertical:"bottom",horizontal:"right"}}
-        transformOrigin={{vertical:"top",horizontal:"right"}}>
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+      >
         <MenuItem onClick={handleDeleteComment}>Delete</MenuItem>
-        </Menu>
-        </> 
-    );
+      </Menu>
+    </>
+  );
 };
 export default Comments;
--- client/src/components/menu/MainMenu.jsx
+++ client/src/components/menu/MainMenu.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import { Menu, MenuItem } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleMainMenu,toggleColorMode } from '../../redux/slice';
+import { Menu, MenuItem } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { toggleColorMode, toggleMainMenu } from "../../redux/slice";
+import { useLogoutMeMutation } from "../../redux/service";
+import { useEffect } from "react";
+import { Bounce, toast } from "react-toastify";
 
 const MainMenu = () => {
-  const { anchorEl } = useSelector((state) => state.service);
+  const { anchorE1, myInfo } = useSelector((state) => state.service);
+
+  const [logoutMe, logoutMeData] = useLogoutMeMutation();
+
   const dispatch = useDispatch();
 
   const handleClose = () => {
@@ -17,24 +22,54 @@ const MainMenu = () => {
     dispatch(toggleColorMode());
   };
 
-  const handleLogout = () => {
-    // logic for logout
+  const handleLogout = async () => {
+    handleClose();
+    await logoutMe();
   };
 
+  useEffect(() => {
+    if (logoutMeData.isSuccess) {
+      toast.warning(logoutMeData.data.msg, {
+        position: "top-center",
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "colored",
+        transition: Bounce,
+      });
+    }
+    if (logoutMeData.isError) {
+      toast.error(logoutMeData.error.data.msg, {
+        position: "top-center",
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "colored",
+        transition: Bounce,
+      });
+    }
+  }, [logoutMeData.isSuccess, logoutMeData.isError]);
+
   return (
-    <Menu
-      anchorEl={anchorEl}
-      open={Boolean(anchorEl)}
-      onClose={handleClose}
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-    >
-      <MenuItem onClick={handleToggleTheme}>Toggle Theme</MenuItem>
-      <MenuItem component={Link} to="/profile/threads/2">
-        Profile
-      </MenuItem>
-      <MenuItem onClick={handleLogout}>Logout</MenuItem>
-    </Menu>
+    <>
+      <Menu
+        anchorEl={anchorE1}
+        open={anchorE1 !== null ? true : false}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <MenuItem onClick={handleToggleTheme}>Toggle Theme</MenuItem>
+        <Link to={`/profile/threads/${myInfo?._id}`} className="link">
+          <MenuItem>My Profile</MenuItem>
+        </Link>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
+    </>
   );
 };
 
